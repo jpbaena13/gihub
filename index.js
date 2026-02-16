827036f7c82107f47f71e6fdb91b33825181e7d8
jpbaena13 <jpbaena13@hotmail.com>
2026-02-16 07:14:39 -0500

    Adding factorial function

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const isOdd = (num) => {
   return num % 2 !== 0;
 }
 
+const factorial = (num) => {
+  if (num === 0) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+}
+
 console.log(isOdd(1)); // true
-console.log(isOdd(2));
+console.log(isOdd(2)); // true
 console.log(isOdd(3)); // true
+
+console.log(factorial(0)); // 1
+console.log(factorial(1)); // 1
+console.log(factorial(5)); // 120
